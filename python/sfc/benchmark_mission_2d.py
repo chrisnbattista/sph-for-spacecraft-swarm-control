49021b1271060bffb1d48861d5d5c60ba4ff6320
Chris Battista <c.battista@outlook.com>
2021-02-17 11:22:24 -1000

    fixed context on 1d orbit

--- python/sfc/benchmark_mission_2d.py
+++ python/sfc/benchmark_mission_2d.py
@@ -6,12 +6,16 @@ print("Seeding initial state...")
 world = worlds.World(
     initial_state=experiments.initialize_random_circle(n_particles=5, radius=15, min_dist=3),
     n_agents=5,
+    timestep=0.001,
     forces=[
-        lambda x: forces.gravity_well(x, 500),
-        lambda x: forces.world_pressure_force(x, h=1, pressure=0.0001),
+        lambda x, context: forces.gravity_well(x, 500, context=context),
+        lambda x, context: forces.world_pressure_force(x, h=1, pressure=0.00001, context=context),
         ##lambda x: forces.world_viscosity_force(x, h=5),
-        lambda x: forces.viscous_damping_force(x, 80)
-        ]
+        lambda x, context: forces.viscous_damping_force(x, 80, context=context)
+        ],
+    context={
+        'sph-lead': 0
+    }
 )
 
 print("Initializing visualization...")
