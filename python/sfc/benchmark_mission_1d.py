112c70bfacb29522819c20f64cc8dc95b9b496b8
Lillian <lshiba@hawaii.edu>
2021-02-24 00:45:25 -1000

    draft adding circles

--- python/sfc/benchmark_mission_1d.py
+++ python/sfc/benchmark_mission_1d.py
@@ -4,7 +4,7 @@
 
 
 
-from  multi_agent_kinetics import experiments, viz, worlds, forces
+from  multi_agent_kinetics import experiments, viz, worlds, forces, kernels
 
 print("Initiating benchmark mission...")
 
@@ -15,11 +15,8 @@ world = worlds.World(
     n_agents=5,
     forces=[
         lambda x: forces.gravity_well(x, 200),
-<<<<<<< HEAD
         lambda x: forces.world_pressure_force(x, h=1, pressure = 0.001),
-=======
         lambda x: forces.world_pressure_force(x, h=1, pressure=0.0001),
->>>>>>> deed79696481864734d5ff8e9495c9dac6f724b4
         ###lambda x: forces.world_viscosity_force(x, h=5),
         lambda x: forces.viscous_damping_force(x, 150)
         ]
@@ -28,20 +25,22 @@ world = worlds.World(
 print("Initializing visualization...")
 fig, ax = viz.set_up_figure(title="Benchmark Mission 1 with Smoothed Particle Hydrodynamics")
 
+#kernels.gaussian_function(r = 5, h = 10, a = 10, b = 0, c = 1)
 print("Starting sim...")
 for i in range(1000):
 
-    world.advance_state()
-    print(world.get_state())
-    viz.render_1d_orbit_state(world.get_state(), fig, ax)
+     world.advance_state()
+     print(world.get_state())
+     viz.render_1d_orbit_state(world.get_state(), fig, ax)
 
-    viz.render_1d_orbit_state(
-        world.get_state(),
-        fig,
-        ax,
-        agent_colors=['k']*4+['b'],
-        agent_sizes=[100]*5
-    )
+     viz.render_1d_orbit_state(
+         world.get_state(),
+         fig,
+         ax,
+         h,
+         agent_colors=['k']*4+['b'],
+         agent_sizes=[100]*5
+     )
 
     # viz.render_state(
     #     world.get_state(),
