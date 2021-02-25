d440a9950c6e87f06c59b8da81a9d9af973a26ca
Chris Battista <c.battista@outlook.com>
2021-02-24 22:47:33 -1000

    interaction lines

--- python/sfc/benchmark_mission_2d.py
+++ python/sfc/benchmark_mission_2d.py
@@ -2,6 +2,7 @@
 
 N_PARTICLES = 15
 LEADER = N_PARTICLES - 1
+H = 1
 
 
 import numpy as np
@@ -16,12 +17,12 @@ print("Initiating benchmark mission...")
 
 print("Seeding initial state...")
 world = worlds.World(
-    initial_state=experiments.initialize_random_circle(n_particles=N_PARTICLES, radius=N_PARTICLES*1.2, min_dist=3),
+    initial_state=experiments.initialize_random_circle(n_particles=N_PARTICLES, radius=N_PARTICLES*1.2, min_dist=2),
     n_agents=N_PARTICLES,
     timestep=0.001,
     forces=[
         lambda x, context: forces.linear_attractor(x, 500, target=LEADER, context=context),
-        lambda x, context: forces.world_pressure_force(x, h=1, pressure=0.001, context=context),
+        lambda x, context: forces.world_pressure_force(x, h=H, pressure=0.001, context=context),
         ##lambda x: forces.world_viscosity_force(x, h=5),
         lambda x, context: forces.viscous_damping_force(x, 80, context=context),
         lambda x, context: forces.swarm_leader_force(x, np.array([200, 200]), context=context)
@@ -39,14 +40,6 @@ print("Initializing visualization...")
 fig, ax = viz.set_up_figure(title="Benchmark Mission 1 with Smoothed Particle Hydrodynamics")
 fig2, ax2 = viz.set_up_figure(title="Benchmark Mission 1 – 2D plot")
 
-# example data for particle id, mass, position, velocity, and time of data
-sample_data = np.array([
-    [0, 10, 50, 9, 1],
-    [1, 10, 150, 9, 1],
-    [2, 10, 300, 9, 1],
-    [3, 10, 720, 9, 1],
-    [4, 10, 1000, 9, 1],
-])
 print("Starting sim...")
 
 for i in range(10000):
@@ -55,7 +48,7 @@ for i in range(10000):
     state = world.get_state()
 
     # HCL
-    #neighbor_location = np.zeros((3,1))
+    neighbor_location = np.zeros((3,1))
     #std = np.zeros((3,1))
 
     #dead_reckoning_estimates = [state_estimator.eif_dr(agents[i], world.timestep_length, np.append(state[i,5:7], 0).T, std) for i in range(N_PARTICLES)]
@@ -71,7 +64,8 @@ for i in range(10000):
             fig2,
             ax2,
             agent_colors=['k']*(N_PARTICLES-1)+['b'],
-            agent_sizes=[100]*N_PARTICLES
+            agent_sizes=[100]*N_PARTICLES,
+            h=2*H
         )
         viz.render_projected_2d_orbit_state(
             state,
