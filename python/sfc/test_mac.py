a1cce6837182d60537619b089364428e43cf4c49
Lillian <lshiba@hawaii.edu>
2021-04-12 11:41:57 -1000

    unit test draft

--- python/sfc/test_mac.py
+++ python/sfc/test_mac.py
@@ -1,2 +1,19 @@
 import mac_python_script
+import unittest, math
+
+class TestMacPython(unittest.TestCase):
+    def test_quadratic_scaling_factor_3d(self):
+        # 1) Define the input 
+        h = 2
+        # 2) Call the function 
+        result = mac_python_script.quadratic_scaling_factor_3d(h)
+        # 3) Verify the output
+        self.assertEqual(result, 3.0/16/math.pi * h**3)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
 
